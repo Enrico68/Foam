07880172db3b49a17fd45ce8f0e75757e06efe56
Enrico68 <enrico.pirani@gmail.com>
2024-05-23 13:19:15 +0200

    commit

--- Statistik_mit_R/Daten_Manipulation/Übungen/Übung_1.r
+++ Statistik_mit_R/Daten_Manipulation/Übungen/Übung_1.r
@@ -7,11 +7,26 @@ bank <- read.csv("/Users/enricopirani/Foam/Statistik_mit_R/Datasets/bankloan.csv
 names(bank)
 View(bank)
 
-library(dplyr)
+#a. Nur verheiraten Personen 
 bank1 <- select(bank, marital,)
 View(bank1)
 demo3 <- filter(bank1, marital == "married")
 View(demo3)
 
-bank1 <- select(bank, marital, education )
-demo2 <- filter(bank1,marital == "married" & education)
+#b. Verheiratete Personen mit Sekundär- und Tertiärbildungsniveau 
+bank1 <- select(bank, marital, education,)
+View(bank1)
+demo2 <- filter(bank1,marital == "married" & (education == "primary" | education == "tertiary" ))
+View(demo2)
+
+#c. Nur Probanden mehr als 40 Jahren alt
+
+bank1 <- filter(bank, age > 40 )
+View(bank1)
+
+#d. Nur Personen, die einen Kredit und einen Saldo ¨ber 1000 haben 
+
+bank2 <- select(bank, balance, loan,)
+View(bank2)
+bank3 <- filter(bank2,  balance > 1000)
+View(bank3)
